9e0813ba1dbbbfd1138387f79597a0757ef53427
Manish <manishguptha218@gmail.com>
2026-05-13 16:12:58 +0530

    fix: refactor configuration loading for cross-environment compatibility

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -1,24 +1,28 @@
 package utilities;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
 
     private static Properties properties = new Properties();
-    private static final String CONFIG_FILE_PATH = ".\\src\\test\\resources\\config.properties";
 
     static {
-        try (FileInputStream fileInputStream = new FileInputStream(CONFIG_FILE_PATH)) {
-            properties.load(fileInputStream);
-        } catch (IOException e) {
+        try (InputStream input = ConfigReader.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("config.properties file not found in classpath");
+            }
+
+            properties.load(input);
+
+        } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to load config.properties file");
         }
     }
-
-    public ConfigReader() {}
 
     public static String getProperty(String key) {
         return properties.getProperty(key);
